b4a6a4067f911e826e8211c3a8f14ab6cc899271
Camellia <56206613+sialiss@users.noreply.github.com>
2022-01-17 19:56:17 +0300

    change form css

--- src/CreateCards.js
+++ src/CreateCards.js
@@ -23,17 +23,20 @@ function CreateCards(props) {
 	
 	return (		
 		<form className='CreateCards' onSubmit={ handleSubmit }>
-			
-			<input  className='cardTitle' value={cardTitle} onChange={e => setCardTitle(e.target.value)}/>
-			<input className='cardDescription' value={cardDescription} onChange={e => setCardDescription(e.target.value)}/>
-				
-			<div className='buttons'>
+			<div className='wrapper'>
+				<label className='labels'>Title: </label>
+				<input className='cardTitle' value={cardTitle} onChange={e => setCardTitle(e.target.value)} />
+
+				<label className='labels'>Description: </label>
+				<input className='cardDescription' value={cardDescription} onChange={e => setCardDescription(e.target.value)} />
+					
+				<div className='buttons'>
 				<input type="submit" className="confirm" value="✓"/>
 
 				<input type="button" className="delete" value="☓" onClick={ clearInput } />
+				</div>
 			</div>
 		</form>
 	);
 }
-
 export default CreateCards;
