277dad4b11dd9fd43854e94d55ce4af01a37c011
Camellia <56206613+sialiss@users.noreply.github.com>
2022-02-19 21:06:11 +0300

    add correct closing editing form

--- src/Card.js
+++ src/Card.js
@@ -17,22 +17,27 @@ function Card({ data, deleteCard, editCard }) {
     const [isEditing, setIsEditing] = useState([])
 
     function editData(data) {
-        document.forms["editable data"].children['editable text'].classList.toggle("hidden")
+        document.forms["editable data"].children['editable part'].classList.toggle("hidden")
         document.forms["editable data"].children['editing data'].classList.toggle("hidden")
 
         // editCard()
+    }
+
+    function closeEditing() {
+        document.forms["editable data"].children['editable part'].classList.toggle("hidden")
+        document.forms["editable data"].children['editing data'].classList.toggle("hidden")
     }
 
     function camelCase(text) {
         // преобразование текста в CamelCase
     }
 
-    // const handleSubmit = (e) => {
-	// 	// предотвращает обновление страницы (поведение по умолчанию)
-	// 	// создаёт карточку, добавляет карточку в пропс, очищает инпуты
-	// 	e.preventDefault()
-	// 	// сделать перевод фокуса на инпут названия карточки
-	// }
+    const handleSubmit = (e) => {
+		// предотвращает обновление страницы (поведение по умолчанию)
+		// создаёт карточку, добавляет карточку в пропс, очищает инпуты
+		e.preventDefault()
+		// сделать перевод фокуса на инпут названия карточки
+	}
 
     camelCase(data.list)
 
@@ -46,24 +51,31 @@ function Card({ data, deleteCard, editCard }) {
             <div className={["Card", data.list].join(' ')}>
                 {/* Выводит айди, название и описание карточки, а также кнопки редактирования и удаления */}
                 <p className='text'>id: {data.id}</p>
-                <form name='editable data'>
-                    <div name='editable text'>
+                <form name='editable data' onSubmit={ handleSubmit }>
+                    <div name='editable part'>
                         <p className='text'>{ data.title }</p>
-                        <p className='text'>{ data.description }</p>
+                        <p className='text'>{data.description}</p>
+                        <div className='buttons'>
+                            <button className="change" onClick={() => editData(data)}>
+                            ✎
+                            </button>
+                            <button className="delete" onClick={() => deleteCard(data)}>
+                            ☓
+                            </button>
+                        </div>
                     </div>
                     <div name='editing data' className={["wrapper", "hidden"].join(" ")}>
                         <input className='cardTitle' value={data.title} onChange={e => data.title = e.target.value} />
                         <input className='cardDescription' value={data.description} onChange={e => data.description = e.target.value} />
+                        <div className='buttons'>
+                            {/* кнопка сабмит по нажатию сабмитит форму (энтер в инпуте описания тоже) */}
+                            <input type="submit" className="confirm" value="✓"/>
+                            
+                            {/* просто чистит инпуты */}
+                            <input type="button" className="delete" value="☓" onClick={ closeEditing } />
+				        </div>
                     </div>
                 </form>
-                <div className='buttons'>
-                    <button className="change" onClick={() => editData(data)}>
-                    ✎
-                    </button>
-                    <button className="delete" onClick={() => deleteCard(data)}>
-                    ☓
-                    </button>
-                </div>
             </div>
         </div >
         
